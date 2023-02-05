e9cf548826f5302d604f49e540dd8ac6e11081a0
chinzorigtbs <apple@Macbooks-MacBook-Pro.local>
2023-02-05 17:17:33 +0800

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'dd7380679a90b93b1481339275a26c28'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '232c92e4-47ad-4046-a57d-b78e463a6371'
+};
